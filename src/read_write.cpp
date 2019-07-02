c41f110630011aa344c24db3ad81d91f2a209159
aaceves <aaceves@users.noreply.github.com>
2019-07-02 16:33:00 -0500

    Update read_write.cpp

--- src/read_write.cpp
+++ src/read_write.cpp
@@ -1,198 +1,104 @@
-
-
-#include <fcntl.h>
-#include <termios.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "ros/ros.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"     // Uses Dynamixel SDK library
+#include "usefull_functions.cpp"             // Extra functions: getch()  &  kbhit()
 
-#include "dynamixel_sdk/dynamixel_sdk.h"             // Uses Dynamixel SDK library
+// 1.- Global variables
+#define DXL_ID                          1    // Dynamixel ID
+#define DXL_MOVING_STATUS_THRESHOLD     10   // Dynamixel moving status threshold
 
-// Control table address
-#define ADDR_MX_TORQUE_ENABLE           24                  // Control table address is different in Dynamixel model
-#define ADDR_MX_GOAL_POSITION           30
-#define ADDR_MX_PRESENT_POSITION        36
+dynamixel::PortHandler *portHandler;         // Port handler
+dynamixel::PacketHandler *packetHandler;     // Packet handler
 
-// Protocol version
-#define PROTOCOL_VERSION                1.0                 // See which protocol version is used in the Dynamixel
+int indexs = 0;
+int dxl_goal_position[2] = {100, 4000};      // Goal position
+int dxl_comm_result = COMM_TX_FAIL;          // Communication result
+uint8_t dxl_error = 0;                       // Dynamixel error
+uint16_t dxl_present_position;               // Present position
 
-// Default setting
-#define DXL_ID                          1                   // Dynamixel ID: 1
-#define BAUDRATE                        1000000             // 57600
-#define DEVICENAME                      "/dev/ttyUSB0"      // Check which port is being used on your controller
-                                                            // ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
-#define TORQUE_ENABLE                   1                   // Value for enabling the torque
-#define TORQUE_DISABLE                  0                   // Value for disabling the torque
-#define DXL_MINIMUM_POSITION_VALUE      100                 // Dynamixel will rotate between this value
-#define DXL_MAXIMUM_POSITION_VALUE      4000                // and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
-#define DXL_MOVING_STATUS_THRESHOLD     10                  // Dynamixel moving status threshold
 
-#define ESC_ASCII_VALUE                 0x1b
+// User-functions that utilize global variables
 
-int getch()
-{
-  struct termios oldt, newt;
-  int ch;
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  ch = getchar();
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  return ch;
-}
-
-int kbhit(void)
-{
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
-
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-  ch = getchar();
-
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-  if (ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
-
-  return 0;
-}
-
-int main(int argc, char **argv)
-{
-  // Initialize PortHandler instance
-  // Set the port path
-  // Get methods and members of PortHandlerLinux or PortHandlerWindows
-  ros::init(argc, argv, "read_write");
-  ros::NodeHandle n;
-  
-  dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
-
-  // Initialize PacketHandler instance
-  // Set the protocol version
-  // Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
-  dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
-
-  int index = 0;
-  int dxl_comm_result = COMM_TX_FAIL;             // Communication result
-  int dxl_goal_position[2] = {DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE};         // Goal position
-
-  uint8_t dxl_error = 0;                          // Dynamixel error
-  uint16_t dxl_present_position = 0;              // Present position
-
-  // Open port
-  if (portHandler->openPort())
-  {
-    printf("Succeeded to open the port!\n");
-  }
-  else
-  {
-    printf("Failed to open the port!\n");
-    printf("Press any key to terminate...\n");
-    getch();
-    return 0;
-  }
-
-  // Set port baudrate
-  if (portHandler->setBaudRate(BAUDRATE))
-  {
-    printf("Succeeded to change the baudrate!\n");
-  }
-  else
-  {
-    printf("Failed to change the baudrate!\n");
-    printf("Press any key to terminate...\n");
-    getch();
-    return 0;
-  }
-
-  // Enable Dynamixel Torque
-  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS)
-  {
-    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-  }
-  else if (dxl_error != 0)
-  {
-    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
-  }
-  else
-  {
-    printf("Dynamixel has been successfully connected \n");
-  }
-
-  while(1)
-  {
-    printf("Press any key to continue! (or press ESC to quit!)\n");
-    if (getch() == ESC_ASCII_VALUE)
-      break;
-
-    // Write goal position
-    dxl_comm_result = packetHandler->write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, dxl_goal_position[index], &dxl_error);
+void read(void) {            // Read present position
+	// Sending Instruction Packet
+    dxl_comm_result = packetHandler->read2ByteTxRx(portHandler, DXL_ID, 36, &dxl_present_position, &dxl_error);
+    
+    // Analizing received Status Packet
     if (dxl_comm_result != COMM_SUCCESS)
-    {
-      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-    }
-    else if (dxl_error != 0)
-    {
-      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
-    }
-
-    do
-    {
-      // Read present position
-      dxl_comm_result = packetHandler->read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION, &dxl_present_position, &dxl_error);
-      if (dxl_comm_result != COMM_SUCCESS)
-      {
         printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-      }
-      else if (dxl_error != 0)
-      {
+    else if (dxl_error != 0)
         printf("%s\n", packetHandler->getRxPacketError(dxl_error));
-      }
+}
 
-      printf("[ID:%03d] GoalPos:%03d  PresPos:%03d\n", DXL_ID, dxl_goal_position[index], dxl_present_position);
+void write(void) {        // Write goal position
+	// Sending Instruction Packet
+    dxl_comm_result = packetHandler->write2ByteTxRx(portHandler, DXL_ID, 30, dxl_goal_position[indexs], &dxl_error);
+        
+    // Analizing received Status Packet
+    if (dxl_comm_result != COMM_SUCCESS)
+        printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    else if (dxl_error != 0)
+        printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+}
 
-    }while((abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD));
-
-    // Change goal position
-    if (index == 0)
-    {
-      index = 1;
-    }
+void torque_on(void) {     // Enable Dynamixel Torque
+	// Sending Instruction Packet
+    dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, 24, 1, &dxl_error);
+    
+    // Analizing received Status Packet
+    if (dxl_comm_result != COMM_SUCCESS) 
+        printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    else if (dxl_error != 0)
+        printf("%s\n", packetHandler->getRxPacketError(dxl_error));
     else
-    {
-      index = 0;
+        printf("Torque successfully enabled \n");
+}
+
+void torque_off(void) {    // Disable Dynamixel Torque
+	// Sending Instruction Packet
+    dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, 24, 0, &dxl_error);
+    
+    // Analizing received Status Packet
+    if (dxl_comm_result != COMM_SUCCESS)
+        printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    else if (dxl_error != 0)
+        printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    else
+        printf("Torque successfully disabled \n");
+}
+
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "read_write");
+    ros::NodeHandle n;
+
+    // ---------------------------------- INICIA CODIGO DE DINAMIXELSDK ---------------------------------------------  
+    portHandler = dynamixel::PortHandler::getPortHandler("/dev/ttyUSB0"); // Linux: "/dev/ttyUSB0" 
+    packetHandler = dynamixel::PacketHandler::getPacketHandler(1.0);      // Protocol version
+
+    portHandler->openPort();                 // 2.- Open port
+    portHandler->setBaudRate(1000000);       // 3.- Set baudrate (Ex. 9600, 57600, 1000000)
+    
+    torque_on();                             // 4.- Torque ON command
+    while(true) {
+        printf("Press any key to continue! (or press ESC to quit!)\n");
+        if (getch() == ESC_ASCII_VALUE)    break;
+        write();                             // 4.- Write goal position command
+        do {
+            read();                          // 4.- Read present position command
+            printf("[ID:%03d] GoalPos:%03d  PresPos:%03d\n", DXL_ID, dxl_goal_position[indexs], dxl_present_position);
+        } while(abs(dxl_goal_position[indexs] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD);
+        // Change goal position
+        if (indexs == 0)   indexs = 1;
+        else               indexs = 0;
     }
-  }
-
-  // Disable Dynamixel Torque
-  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS)
-  {
-    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-  }
-  else if (dxl_error != 0)
-  {
-    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
-  }
-
-  // Close port
-  portHandler->closePort();
+    torque_off();                            // 4.- Torque OFF command
+ 
+    // Close port
+    portHandler->closePort();                // 5.- Close port
   
-  ros::spinOnce();
-   
-  return 0;
+    // ---------------------------------- TERMINA CODIGO DE DINAMIXELSDK ---------------------------------------------
+    ros::spinOnce();
+    return 0;
 }
